9ac59b88b6b19ee98729299d01e6472bb9676b33
official-oscar <uwagboeosarugue@gmail.com>
2026-07-10 13:22:31 +0100

    created and wrote code for journal, entry and promptgenerator cs files, and added code to program.cs.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,69 @@
+/*
+ * Author: Osarugue Uwagboe
+ * Project: Week 02 Journal
+ 
+ * 1. Added 5 extra prompts.
+ * 2. Handled error for loading files that don't exist.
+ * 3. Used private member variables in all classes to demonstrate encapsulation/abstraction.
+ */
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string choice = "";
+
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"{prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                string dateText = DateTime.Now.ToShortDateString();
+                Entry newEntry = new Entry(dateText, prompt, response);
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter 1-5.");
+            }
+        }
     }
 }
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    private List<string> _prompts;
+
+    public PromptGenerator()
+    {
+        _prompts = new List<string>()
+        {
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "How did I see the hand of the Lord in my life today?",
+            "What was the strongest emotion I felt today?",
+            "If I had one thing I could do over today, what would it be?",
+            "What did I learn about myself today?",
+            "What am I grateful for right now?",
+            "What got me upset today?",
+            "What made me smile today?",
+            "Did I think to pray when faced with any challenge today?"
+        };
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
